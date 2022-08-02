af918ebc277d07c998c297f3688562bff57bb751
nikichDmITRO <dmitracenkonikita5@gmail.com>
2022-08-02 23:42:51 +0300

    Проверил первый эндпоинт

--- server/index.js
+++ server/index.js
@@ -1,15 +1,32 @@
 import express from "express";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
 
 const app = express();
+dotenv.config();
+
+const PORT = process.env.PORT || 3001;
+const DB_USER = process.env.DB_USER || 3001;
+const DB_PASSWORD = process.env.DB_PASSWORD || 3001;
+const DB_NAME = process.env.DB_NAME || 3001;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/",(req,res)=>{
+res.json({message:"HELLO NIKICH"})
+})
 
 async function start() {
   try {
     await mongoose.connect(
-      "mongodb+srv://adm:123adm123@cluster0.dzddehm.mongodb.net/?retryWrites=true&w=majority"
+      `mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.dzddehm.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`
     );
-    app.listen(3002,()=>console.log(`Server started on port: ${3002}`))
-  } catch (err) {}
+    app.listen(PORT, () => console.log(`Server started port: ${PORT}`));
+  } catch (err) {
+    console.log(err);
+  }
 }
 
-start()
+start();
